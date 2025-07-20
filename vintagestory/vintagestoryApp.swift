4fe838eb6922877dbb3955de0ab583ddfde8b285
jacopouggeri <jacopo.uggeri@gmail.com>
2025-07-21 00:31:07 +0100

    Initial Commit

---
+++ vintagestory/vintagestoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  vintagestoryApp.swift
+//  vintagestory
+//
+//  Created by Jacopo Uggeri on 21/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct vintagestoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
